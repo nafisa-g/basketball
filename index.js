5d2e9b7e003bb8e3d9bb6b86acfde183e4e3d8e9
GAGGUTURI NASEEB JAN <156880544+nafisa-g@users.noreply.github.com>
2024-06-23 18:45:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+let jan=document.getElementById("home-score");
+console.log(jan);
+
+function qwe1() {
+    jan.innerText = Number(jan.innerText)+1;
+}
+function qwe2() {
+    jan.innerText=Number(jan.innerText)+ 2;
+}
+function qwe3() {
+    jan.innerText =Number(jan.innerText) +3;
+}
+
+
+
+let Jan=document.getElementById("guest-score");
+console.log(Jan);
+
+
+function gqwe1() {
+    Jan.innerText= Number(Jan.innerText)+1;
+}
+function gqwe2() {
+    Jan.innerText =Number(Jan.innerText)+2;
+}
+function gqwe3() {
+    Jan.innerText =Number(Jan.innerText)+ 3;
+}
